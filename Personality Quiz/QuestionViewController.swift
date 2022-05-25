86ac65e9ea37239f5afea3bf8832c7fe98cffd62
Mathew Soto <mathewsoto@nypl.org>
2022-05-25 17:41:57 -0400

    day 2 progress

--- Personality Quiz/QuestionViewController.swift
+++ Personality Quiz/QuestionViewController.swift
@@ -8,11 +8,93 @@
 import UIKit
 
 class QuestionViewController: UIViewController {
+    
+    @IBOutlet var singleStackView: UIStackView!
+    @IBOutlet var multipleStackView: UIStackView!
+    @IBOutlet var rangedStackView: UIStackView!
+    
+    @IBOutlet var questionLabel: UILabel!
+    
+    @IBOutlet var singleButton1: UIButton!
+    @IBOutlet var singleButton2: UIButton!
+    @IBOutlet var singleButton3: UIButton!
+    @IBOutlet var singleButton4: UIButton!
+    
+    @IBOutlet var multiLabel1: UILabel!
+    @IBOutlet var multiLabel2: UILabel!
+    @IBOutlet var multiLabel3: UILabel!
+    @IBOutlet var multiLabel4: UILabel!
+    @IBOutlet var multiSwitch1: UISwitch!
+    @IBOutlet var multiSwitch2: UISwitch!
+    @IBOutlet var multiSwitch3: UISwitch!
+    @IBOutlet var multiSwitch4: UISwitch!
+    
+    @IBOutlet var rangedSlider: UISlider!
+    @IBOutlet var rangedLabel1: UILabel!
+    @IBOutlet var rangedLabel2: UILabel!
+    
+    @IBOutlet var questionProgressView: UIProgressView!
+    
+    var questionIndex = 0
+    var questions: [Question] = [
+        Question(
+            text: "Which food do you like the most?",
+            type: .single,
+            answers: [
+                Answer(text: "Steak", type: .dog),
+                Answer(text: "Fish", type: .cat),
+                Answer(text: "Carrots", type: .rabbit),
+                Answer(text: "Corn", type: .turtle)
+            ]
+        ),
+        
+        Question(
+            text: "Which activities do you enjoy?",
+            type: .multiple,
+            answers: [
+                Answer(text: "Swimming", type: .turtle),
+                Answer(text: "Sleeping", type: .cat),
+                Answer(text: "Cuddling", type: .rabbit),
+                Answer(text: "Eating", type: .dog)
+            ]
+        ),
+        
+        Question(
+            text: "How much do you enjoy car rides?",
+            type: .ranged,
+            answers: [
+                Answer(text: "I dislike them", type: .cat),
+                Answer(text: "I get a little nervous", type: .rabbit),
+                Answer(text: "I barely notice them", type: .turtle),
+                Answer(text: "I love them", type: .dog)
+            ]
+        )
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        updateUI()
+    }
+    
+    func updateUI() {
+        singleStackView.isHidden = true
+        multipleStackView.isHidden = true
+        rangedStackView.isHidden = true
+        
+        let currentQuestion = questions[questionIndex]
+        navigationItem.title = "Question #\(questionIndex + 1)"
+        
+        switch currentQuestion.type {
+        case .single:
+            singleStackView.isHidden = false
+        case .multiple:
+            multipleStackView.isHidden = false
+        case .ranged:
+            rangedStackView.isHidden = false
+            
+        }
     }
     
 
